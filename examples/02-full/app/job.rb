4edcfdb04285c1b5d9cf65d22e474775512dfdd0
Danny Ben Shitrit <db@dannyben.com>
2019-03-14 18:21:04 +0000

    remove examples and shrink readme

--- examples/02-full/app/job.rb
+++
@@ -1,9 +0,0 @@
-require 'tty-command'
-
-class Job < Jobly::Job
-  # Provide a common way for the inheriting jobs to run shell commands
-  # and send output to the common logger.
-  def shell
-    @shell ||= TTY::Command.new(output: logger, color: false)
-  end
-end
