8e2b25857900da6fbce64fd751cd35a52dff2d56
Danny Ben Shitrit <db@dannyben.com>
2019-04-16 16:08:50 +0000

    fix cli arg:value-with-colon

--- lib/jobly/refinements/argument_converters.rb
+++ lib/jobly/refinements/argument_converters.rb
@@ -2,7 +2,7 @@ module Jobly
   module ArgumentConverters
     refine Array do
       def to_params
-        map { |pair| pair.split ':' }.to_h.transform_keys(&:to_sym)
+        map { |pair| pair.split ':', 2 }.to_h.transform_keys(&:to_sym)
       end
     end
   end
