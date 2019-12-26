3045ac37ccf04d631cd11e30c8eabd9c6b690b84
Danny Ben Shitrit <db@dannyben.com>
2019-12-26 20:04:13 +0000

    add JOBLY_SHELL_DRY_RUN

--- lib/jobly/helpers/shell.rb
+++ lib/jobly/helpers/shell.rb
@@ -3,7 +3,13 @@ require 'tty-command'
 module Jobly
   module Shell
     def shell
-      @shell ||= TTY::Command.new(output: logger, color: false)
+      @shell ||= shell!
+    end
+
+    def shell!
+      TTY::Command.new output: logger, 
+        color: false,
+        dry_run: !!ENV['JOBLY_SHELL_DRY_RUN']
     end
   end
 end
