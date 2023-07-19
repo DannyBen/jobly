3e60960fc6b66478d48ca3f31f50a81d5c5a5783
J. Brandt Buckley <brandt@runlevel1.com>
2023-07-19 12:31:52 -0600

    Add --host flag to set the server bind address
    This adds a `--host` flag so that you can specify where the jobly server will bind.
    
    Before this change, it was hard-coded to 0.0.0.0 which binds to every available interface. For security, I only want it to listen on localhost. With this change it can be done with: `jobly server --host localhost`:
    
    ```
    $ bundle exec bin/jobly server --host localhost
    Starting server
    Puma starting in single mode...
    * Puma version: 6.3.0 (ruby 3.2.0-p0) ("Mugi No Toki Itaru")
    *  Min threads: 0
    *  Max threads: 5
    *  Environment: development
    *          PID: 36472
    * Listening on http://127.0.0.1:3000
    * Listening on http://[::1]:3000
    Use Ctrl-C to stop
    ...
    ```
    
    I've matched the flag names to what `rackup` uses.

--- spec/jobly/commands/server_cmd_spec.rb
+++ spec/jobly/commands/server_cmd_spec.rb
@@ -3,8 +3,11 @@ require 'spec_helper'
 describe Commands::ServerCmd do
   context 'without arguments' do
     it 'runs the dashboard and api server' do
-      command = %r{rackup --env development --port 3000 --host 0.0.0.0 .*lib/jobly/config\.ru}
-      expect_any_instance_of(described_class).to receive(:exec).with(command)
+      command = [
+        'rackup', '--env', 'development', '--port', '3000',
+        '--host', '0.0.0.0', match(%r{.*/lib/jobly/config.ru})
+      ]
+      expect_any_instance_of(described_class).to receive(:exec).with(*command)
       expect { subject.execute %w[server] }.to output_approval('cli/server/no-args')
     end
   end
